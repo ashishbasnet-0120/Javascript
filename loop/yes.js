61130b8629c16630a54f4da51dd010e0bed1e9a1
ashishbasnet-0120 <basnetashish@gmail.com>
2024-10-20 20:19:59 +0545

    changed

---
+++ loop/yes.js
@@ -0,0 +1,18 @@
+// use of template literals
+let biscuit=100;
+let pop=1000;
+console.log("the total price is:",(biscuit+pop),"rupees.");
+console.log(`the total price is: ${biscuit+pop} rupees.`);
+
+
+
+// operators in js
+let a=5;
+let b=10;
+console.log(a+b);
+console.log(a**b);
+
+// unary operator 
+console.log(a++);// output is 5 because first of all the value of a is store then uodate is done
+
+console.log(++b);// output is 11 because first of all the value is update and store
